540700ca89bee6d5080ecc9518966c7d5413ecc7
Manas Mishra <163861485+Manasm149@users.noreply.github.com>
2026-08-10 21:09:04 +0530

    Step 22: action_mask

--- model.py
+++ model.py
@@ -263,8 +263,16 @@ def policy_value_forward(net, encoded_board):
 
     return logits, value
 
-# Step 22 - action_mask (not yet solved)
-# TODO: implement
+# Step 22 - action_mask
+import numpy as np
+
+def action_mask(board):
+    mask = np.zeros(7, dtype=bool)
+
+    for column in valid_moves(board):
+        mask[column] = True
+
+    return mask
 
 # Step 23 - masked_policy_logits (not yet solved)
 # TODO: implement
