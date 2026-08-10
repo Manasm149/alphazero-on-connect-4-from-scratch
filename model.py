e9067db3eb2794711c9c8051b8e3536c6d20c7d6
Manas Mishra <163861485+Manasm149@users.noreply.github.com>
2026-08-10 20:59:54 +0530

    Step 9: four_in_a_row_diagonal_up_right

--- model.py
+++ model.py
@@ -78,8 +78,18 @@ def four_in_a_row_diagonal_down_right(board):
 
     return 0
 
-# Step 9 - four_in_a_row_diagonal_up_right (not yet solved)
-# TODO: implement
+# Step 9 - four_in_a_row_diagonal_up_right
+def four_in_a_row_diagonal_up_right(board):
+    for row in range(3, 6):
+        for col in range(4):
+            if (board[row, col] != 0 and
+                board[row, col] == board[row - 1, col + 1] and
+                board[row, col] == board[row - 2, col + 2] and
+                board[row, col] == board[row - 3, col + 3]):
+
+                return int(board[row, col])
+
+    return 0
 
 # Step 10 - check_winner (not yet solved)
 # TODO: implement
